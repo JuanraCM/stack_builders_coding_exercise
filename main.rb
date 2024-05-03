aaca19767874e0ccf9426d92e9f992b2b181db05
JuanraCM <juanracm.dev@gmail.com>
2024-05-03 09:59:26 +0100

    Initial commit

---
+++ main.rb
@@ -0,0 +1,5 @@
+# TODO: Crawl the first 30 entries from https://news.ycombinator.com/. Only care about the title, the number of the order, the number of comments, and points for each entry
+
+# TODO: Filter entries with more than 5 words in the title by the number of comments first
+
+# TODO: Filter entries with less than or equal to 5 words in the title ordered by points
