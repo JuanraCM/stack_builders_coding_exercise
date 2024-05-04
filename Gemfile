d4b6649ace74ce223021b7ed19a4386daf249d6e
JuanraCM <juanracm.dev@gmail.com>
2024-05-04 12:59:07 +0100

    feat(tests): add webmock & vcr gems to avoid real HTTP requests

--- Gemfile
+++ Gemfile
@@ -10,3 +10,9 @@ gem 'rubocop', require: false
 
 # Testing library
 gem 'rspec'
+
+# Mock HTTP requests for tests
+gem 'webmock'
+
+# Record and replay HTTP interactions during tests
+gem 'vcr'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+
+require 'webmock/rspec'
+require 'vcr'
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -95,4 +99,9 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+end
+
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/fixtures'
+  config.hook_into :webmock
 end
